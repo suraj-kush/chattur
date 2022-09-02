afb9f2bd8e607f135ca6cf2d24ee7ed757d9e888
sidkhuntia <siddharthakhuntia@gmail.com>
2022-09-03 01:19:58 +0530

    fix: index bug

--- client/src/index.js
+++ client/src/index.js
@@ -2,14 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
-import { AuthProvider } from "./context/AuthContext";
 import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
-  <AuthProvider>
     <Router>
       <App />
-    </Router>
-  </AuthProvider>,
+    </Router>,
   document.getElementById("root")
 );
