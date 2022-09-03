cddd806c3aebd72fcbb3ea96c08ab54c23eb4692
sidkhuntia <siddharthakhuntia@gmail.com>
2022-09-03 12:00:54 +0530

    feat: scoket stream add

--- client/src/pages/Room.jsx
+++ client/src/pages/Room.jsx
@@ -5,6 +5,7 @@ import { io } from 'socket.io-client'
 import Peer from 'simple-peer'
 
 import Loading from '../components/Loading'
+import { useAuth } from '../middleware/authentication'
 
 //sounds
 import joinSoundSrc from "../sounds/join.mp3"
@@ -14,8 +15,11 @@ const Room = () => {
   const [loading, setLoading] = useState(true)
   const [msgText, setMsgText] = useState('')
   const [msgs, setMsgs] = useState([])
+  const [localStream, setLocalStream] = useState(null)
   const { roomID } = useParams()
   const socket = useRef()
+
+  const { user, login } = useAuth();
 
   //functions
   const sendMessage = (e) => {
@@ -25,7 +29,9 @@ const Room = () => {
         roomID,
         from: socket.current.id,
         user: {
-          // send user from fribase
+            id: user.uid,
+            name: user?.displayName,
+            profilePic: user.photoURL,
         },
         message: msgText.trim(),
       })
@@ -47,6 +53,10 @@ const Room = () => {
         user: user
           ? {
               //user from firebase
+              uid: user?.uid,
+              email: user?.email,
+              name: user?.displayName,
+              photoURL: user?.photoURL,
             }
           : null,
       })
@@ -95,10 +105,76 @@ const Room = () => {
                 audio: true
             })
             .then((stream)){
-
+                setLoading(false);
+                setLocalStream(stream);
+                localVideo.current.srcObject = stream;
+                socket.current.emit("join room", {
+                  roomID,
+                  user: user
+                    ? {
+                        uid: user?.uid,
+                        email: user?.email,
+                        name: user?.displayName,
+                        photoURL: user?.photoURL,
+                      }
+                    : null,
+                });
+                socket.current.on("all users", (users) => {
+                    const peers = [];
+                    users.forEach((user) => {
+                      const peer = createPeer(user.userId, socket.current.id, stream);
+                      peersRef.current.push({
+                        peerID: user.userId,
+                        peer,
+                        user: user.user,
+                      });
+                      peers.push({
+                        peerID: user.userId,
+                        peer,
+                        user: user.user,
+                      });
+                    });
+                    setPeers(peers);
+                  });
+    
+                socket.current.on("user joined", (payload) => {
+                  // console.log(payload);
+                  const peer = addPeer(payload.signal, payload.callerID, stream);
+                  peersRef.current.push({
+                    peerID: payload.callerID,
+                    peer,
+                    user: payload.user,
+                  });
+    
+                  const peerObj = {
+                    peerID: payload.callerID,
+                    peer,
+                    user: payload.user,
+                  };
+    
+                  setPeers((users) => [...users, peerObj]);
+                });
+    
+                socket.current.on("receiving returned signal", (payload) => {
+                  const item = peersRef.current.find(
+                    (p) => p.peerID === payload.id
+                  );
+                  item.peer.signal(payload.signal);
+                });
+    
+                socket.current.on("user left", (id) => {
+                  const audio = new Audio(leaveSFX);
+                  audio.play();
+                  const peerObj = peersRef.current.find((p) => p.peerID === id);
+                  if (peerObj) peerObj.peer.destroy();
+                  const peers = peersRef.current.filter((p) => p.peerID !== id);
+                  peersRef.current = peers;
+                  setPeers((users) => users.filter((p) => p.peerID !== id));
+                });
             }
         }
     }
+    return video();
   },[roomID])
 
   return (
