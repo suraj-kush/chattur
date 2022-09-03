b2293c8c865ee639d325c0aef75fd16b7e0ceb59
sidkhuntia <siddharthakhuntia@gmail.com>
2022-09-03 19:10:02 +0530

    asd

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 
 import { LogOutIcon } from "../Icons"
 
-import { useAuth } from "../middleware/authentication"
+import { useAuth } from "../middleware/Authentication"
 import SignIn from "./SignIn"
 
 const Header = () => {
