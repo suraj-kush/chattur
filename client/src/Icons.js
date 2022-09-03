567c9905d75cf5e3aff6664e771baa18a85133f6
scarface68 <saichandan68@gmail.com>
2022-09-03 12:10:54 +0530

    updated comp

--- client/src/Icons.js
+++ client/src/Icons.js
@@ -1,6 +1,13 @@
 import AiOutlineLogout from "react-icons/ai";
 import IoSettingsOutline from "react-icons/io";
+import {IoMic, IoMicOff} from "react-icons/io5";
+import {BsPin, BsPinFill} from "react-icons/bs";
+
 
 
 export const LogOutIcon = () => (<AiOutlineLogout />);
 export const SettingsIcon = () => (<IoSettingsOutline />);
+export const MicOnIcon = () => (<IoMic />);
+export const MicOffIcon = () => (<IoMicOff />);
+export const PinIcon = () => (<BsPin />);
+export const PinActiveIcon = () => (<BsPinFill />);
